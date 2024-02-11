58b819060b113c14af0c20c479553cc177431dfe
Hacker94 <83153199+Ketan-vscode@users.noreply.github.com>
2024-02-11 16:27:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 status = "";
+results = [];
 
  function preload(){
     img = loadImage("");
@@ -16,4 +17,17 @@ function gotResult(error, results){
         console.log(error);
     }
     console.log(results);
+}
+
+function draw(){
+    
+    if(status != "")
+    {
+        for(i = 0; i< objects.length; i++){
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
 }
